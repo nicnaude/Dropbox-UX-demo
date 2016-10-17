34adac7076de0b62deaa58130af293378c9ca992
Nicholas Naudé <nicnaude@gmail.com>
2016-10-16 18:35:07 -0700

    Completes user flow through new and existing user flow.

---
+++ SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  Dropbox
+//
+//  Created by Nicholas Naudé on 16/10/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: ViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: 375, height: 1200)
+    }
+}
